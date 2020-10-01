8204a9c765ee88499551a66fd54b3ec248f53877
ksalrin <salrindesigngeek@gmail.com>
2020-10-01 23:16:45 +0000

    updated for testing ticket 54

--- JenkinsBuilder.groovy
+++ JenkinsBuilder.groovy
@@ -1,3 +1,3 @@
-@Library('CommonLib@master') _
+@Library('CommonLib@feature/54') _
 def common = new com.lib.JenkinsCommonDockerBuildPipeline()
 common.runPipeline()
--- JenkinsDeployer.groovy
+++ JenkinsDeployer.groovy
@@ -1,3 +1,3 @@
-@Library('CommonLib@master') _
+@Library('CommonLib@feature/54') _
 def common = new com.lib.JenkinsCommonDeployPipeline()
 common.runPipeline()
